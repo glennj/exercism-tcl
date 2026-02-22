ca332995c5621f491ca58aa07ceabf134425ffef
Glenn Jackman <glenn.jackman@gmail.com>
2026-02-22 14:05:28 -0500

    does not need OO

--- exercises/practice/prism/.meta/example.tcl
+++ exercises/practice/prism/.meta/example.tcl
@@ -17,118 +17,77 @@ proc ::tcl::mathfunc::roundTo {n pts} {
 }
 
 ############################################################
-oo::class create Point {
-    variable x y
+# If you have a starting point $P_1(x_1, y_1)$ and an angle
+# $\theta$, the slope ($m$) of the line is the tangent of that
+# angle:$$m = \tan(\theta)$$
+# The equation for any other point $(x, # y)$ on that line is:
+# $$y - y_1 = m(x - x_1)$$
+#
+proc isInline {line px py} {
+    lassign $line lx ly angle
 
-    constructor {_x _y} {
-        set x $_x
-        set y $_y
-    }
+    set deltaX [expr {$px - $lx}]
+    set deltaY [expr {$py - $ly}]
 
-    method x {} {return $x}
-    method y {} {return $y}
-    method toString {} {return "($x,$y)"}
-
-    # If you have a starting point $P_1(x_1, y_1)$ and an angle
-    # $\theta$, the slope ($m$) of the line is the tangent of that
-    # angle:$$m = \tan(\theta)$$
-    # The equation for any other point $(x, # y)$ on that line is:
-    # $$y - y_1 = m(x - x_1)$$
-    #
-    method isInline {line} {
-        set deltaX [expr {$x - [[$line point] x]}]
-        set deltaY [expr {$y - [[$line point] y]}]
-
-        if {$deltaX == 0 && $deltaY == 0} {
-            return false
-        } elseif {[$line isVertical]} {
-            expr {$deltaX == 0}
-        } else {
-            expr {approxEqual($deltaY, [$line slope] * $deltaX, 0.06)}
-        }
+    if {$deltaX == 0 && $deltaY == 0} {
+        # reject the line's point
+        return false
+    } elseif {approxEqual(abs(fmod($angle, 180)), 90, 0.002)} { 
+        # vertical line?
+        expr {$deltaX == 0}
+    } else {
+        set slope [expr {tan(deg2rad($angle))}]
+        expr {approxEqual($deltaY, $slope * $deltaX, 0.06)}
     }
 }
 
-############################################################
-oo::class create Line {
-    variable point deg theta
+proc nextPrism {line prisms} {
+    lassign $line lx ly angle
+    set theta [expr {deg2rad($angle)}]
 
-    constructor {p angle} {
-        set point $p
-        set deg [expr {fmod($angle, 360)}]
-        set theta [expr {deg2rad($angle)}]
-    }
+    set min [expr {(1 << 63) - 1}] ;# "max int"
+    set next ""
 
-    method point {} {return $point}
-    method angle {} {return $deg}
-    method toString {} {return "Line{[$point toString] $deg}"}
-
-    method isVertical {} {
-        expr {approxEqual(abs(fmod($deg, 180)), 90, 0.002)}
-    }
-
-    method slope {} {
-        expr {tan($theta)}
-    }
-
-    method nextPrism {prisms} {
-        set min [expr {(1 << 63) - 1}] ;# "max int"
-        set next ""
-
-        foreach p $prisms {
-            set deltaX [expr {[[$p point] x] - [[my point] x]}]
-            set deltaY [expr {[[$p point] y] - [[my point] y]}]
-            set projection [expr {$deltaX * cos($theta) + $deltaY * sin($theta)}]
+    foreach p $prisms {
+        lassign [lrange $p 1 2] px py
+        if {[isInline $line $px $py]} {
+            # the dot product of the angle vector and the distance vector
+            set projection [expr {($px - $lx) * cos($theta) + ($py - $ly) * sin($theta)}]
+            # the projection must be positive for the next point to be "ahead"
+            # of the line point's direction.
             if {$projection > 0 && $projection < $min} {
                 set min $projection
                 set next $p
             }
         }
-
-        return $next
     }
+
+    return $next
 }
 
-############################################################
-oo::class create Prism {
-    variable name point refraction
-
-    construct {input} {
-        set name [dict get $input id]
-        set point [Point new [dict get $input x] [dict get $input y]]
-        set refraction [dict get $input angle]
-    }
-
-    method point {} {return $point}
-    method name {} {return $name}
-    method toString {} {return "Prism#$name{[$point toString] $refraction}"}
-
-    method refract {angle} {
-        expr {roundTo(fmod($angle + $refraction, 360), 3)}
-    }
+proc refract {angle refraction} {
+    expr {roundTo(fmod($angle + $refraction, 360), 3)}
 }
 
 ############################################################
 proc findSequence {input} {
-    set sp [Point new [dict get $input start x] [dict get $input start y]]
-    set line [Line new $sp [dict get $input start angle]]
+    # a line is a list: {x y angle}
+    set line [dict values [dict get $input start]]
 
-    set prisms [lmap p [dict get $input prisms] {Prism new $p}]
+    # a prism is a list: {id x y refraction}
+    set prisms [lmap p [dict get $input prisms] {dict values $p}]
 
     set result [list]
 
     while {true} {
-        set inlinePrisms [lmap p $prisms {expr {
-            [[$p point] isInline $line] ? $p : [continue]
-        }}]
+        set prism [nextPrism $line $prisms]
 
-        set prism [$line nextPrism $inlinePrisms]
-        if {$prism == ""} then break
+        if {$prism == ""} {
+            return $result
+        }
 
-        lappend result [$prism name]
-
-        set line [Line new [$prism point] [$prism refract [$line angle]]]
+        lassign $prism pid px py refr
+        lappend result $pid
+        set line [list $px $py [refract [lindex $line end] $refr]]
     }
-
-    return $result
 }
